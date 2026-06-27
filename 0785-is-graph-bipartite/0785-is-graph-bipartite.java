52c05f74d984cec3326f197caa1c44c8a0793af1
Aparna Garg <gargaparna78@gmail.com>
2026-06-27 10:30:20 +0530

    Time: 6 ms (10.37%), Space: 47.3 MB (37.65%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,31 +1,40 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         int n = graph.length;
-        int[] colors = new int[n];
-        
+        int m = graph[0].length;
         for (int i = 0; i < n; i++) {
-            if (colors[i] != 0) {
-                continue;
-            }
-            Queue<Integer> queue = new LinkedList<>();
-            queue.add(i);
-            colors[i] = 1;
-            
-            while (!queue.isEmpty()) {
-                int curr = queue.poll();
-                
-                for (int neighbor : graph[curr]) {
-                    if (colors[neighbor] == colors[curr]) {
-                        return false;
-                    }
-                    if (colors[neighbor] == 0) {
-                        colors[neighbor] = -colors[curr];
-                        queue.add(neighbor);
-                    }
-                }
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int neighbour : graph[i]) {
+                adj.get(i).add(neighbour);
             }
         }
-        
+        int V = adj.size();
+        int color[] = new int[V];
+        for (int i = 0; i < V; i++) {
+            color[i] = -1;
+        }
+        for (int i = 0; i < V; i++) {
+            if (color[i] == -1) {
+                if (dfs(i, 0, color, adj) == false)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    boolean dfs(int node, int col, int color[], ArrayList<ArrayList<Integer>> adj) {
+        color[node] = col;
+        for (int i : adj.get(node)) {
+            if (color[i] == -1) {
+                if (dfs(i, 1 - col, color, adj) == false)
+                    return false;
+            } else if (color[i] == col)
+                return false;
+        }
         return true;
     }
 }
