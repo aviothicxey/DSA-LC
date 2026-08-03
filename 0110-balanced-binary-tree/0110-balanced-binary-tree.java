db265b303b0dee307a341cf97b425d3963685d02
Aparna Garg <gargaparna78@gmail.com>
2026-08-03 10:03:05 +0530

    Time: 0 ms (100%), Space: 45.8 MB (23.42%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -17,16 +17,14 @@ class Solution {
     public boolean isBalanced(TreeNode root) {
         return rec(root) != -1;
     }
-    public int rec(TreeNode root){
+    int rec(TreeNode root){
         if(root == null)return 0;
-
         int left = rec(root.left);
         if(left == -1)return -1;
         int right = rec(root.right);
         if(right == -1)return -1;
 
         if(Math.abs(left - right) > 1)return -1;
-
-        return Math.max(left,right) + 1;
+        return Math.max(left,right)+1;
     }
 }
